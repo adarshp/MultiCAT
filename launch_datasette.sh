5f4e89ee32844ac63ee3c891a11f00a6b5559027
Adarsh Pyarelal <adarsh@arizona.edu>
2025-03-09 09:16:52 -0700

    wip

--- launch_datasette.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Shell script for launching a Datasette instance with the MultiCAT database
-
-set -euo pipefail
-datasette -i \
-    multicat.db \
-    --metadata metadata.yml \
